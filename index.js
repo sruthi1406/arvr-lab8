edd9f3b787e91903e8467dedbc16b4362987f3b0
SRUTHI <154534657+sruthi1406@users.noreply.github.com>
2025-04-15 13:22:15 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const path = require('path');
 const app = express();
 // Serve static files from the "public" directory
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname)));
 // Start the server
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
